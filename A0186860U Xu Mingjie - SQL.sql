ace9f437f80e4c4807fb27c54966fca350778f52
xumjdeMacbook <xumj1996@gmail.com>
2021-01-23 02:09:36 +0800

    DB name change

--- A0186860U Xu Mingjie - SQL.sql
+++ A0186860U Xu Mingjie - SQL.sql
@@ -2,21 +2,21 @@
 /* Execution of various SQL commands based on Autism dataset: */
 
 /*     How many middle eastern children show ASD traits ? */
-SELECT count(*) FROM Toddler_Autism_dataset where Ethnicity = 'middle eastern' AND [Class/ASDTraits]='Yes';
+SELECT count(*) FROM ASD where Ethnicity = 'middle eastern' AND [Class/ASDTraits]='Yes';
 /*  result: 96 */  
 
 /*     How many children who have Jaundice show ASD traits ? */
-SELECT count(*) FROM Toddler_Autism_dataset where Jaundice = 'yes' AND [Class/ASDTraits]='Yes';
+SELECT count(*) FROM ASD where Jaundice = 'yes' AND [Class/ASDTraits]='Yes';
 /*  reslut: 215 */
 
 /*     Are ASD traits dependent on hereditary ? Justify . */
-SELECT (SELECT (SELECT count(*) FROM Toddler_Autism_dataset 
+SELECT (SELECT (SELECT count(*) FROM ASD 
 WHERE Family_mem_with_ASD='no' AND [Class/ASDTraits]='Yes')*1.0/(
-SELECT count(*) from Toddler_Autism_dataset WHERE Family_mem_with_ASD='no')) 
+SELECT count(*) from ASD WHERE Family_mem_with_ASD='no')) 
 AS 'ASD proportion in family member without ASD',
-(SELECT (SELECT count(*) FROM Toddler_Autism_dataset 
+(SELECT (SELECT count(*) FROM ASD 
 WHERE Family_mem_with_ASD='yes' AND [Class/ASDTraits]='Yes')*1.0/(
-SELECT count(*) from Toddler_Autism_dataset WHERE Family_mem_with_ASD='yes'))
+SELECT count(*) from ASD WHERE Family_mem_with_ASD='yes'))
 AS 'ASD proportion in family member with ASD';
 /*  both are around 0.7, so the answer is ASD traits is NOT dependent on hereditary */
 
@@ -26,12 +26,12 @@ A.Ethnicity,A.Amount as ASD_Amount,
 B.Amount AS Total_Amount, 
 A.Amount*1.0/B.Amount AS  Proportion 
 FROM(SELECT Ethnicity,count(*) As Amount
-FROM Toddler_Autism_dataset  WHERE [Class/ASDTraits]='Yes'
+FROM ASD  WHERE [Class/ASDTraits]='Yes'
 GROUP by Ethnicity
 ) as A
 LEFT JOIN (
 SELECT Ethnicity,count(*) As Amount
-FROM Toddler_Autism_dataset 
+FROM ASD 
 GROUP by Ethnicity
 ) as B 
 on A.Ethnicity = B.Ethnicity
@@ -45,14 +45,14 @@ A.Ethnicity,A.Amount as ASD_Amount,
 B.Amount AS Total_Amount, 
 A.Amount*1.0/B.Amount AS  Proportion 
 FROM(SELECT Ethnicity,count(*) As Amount
-FROM Toddler_Autism_dataset  WHERE [Class/ASDTraits]='Yes' 
+FROM ASD  WHERE [Class/ASDTraits]='Yes' 
 AND Ethnicity = 'White European' 
 AND Sex = 'f'
 GROUP by Ethnicity
 ) as A
 LEFT JOIN (
 SELECT Ethnicity,count(*) As Amount
-FROM Toddler_Autism_dataset  WHERE  Ethnicity = 'White European' 
+FROM ASD  WHERE  Ethnicity = 'White European' 
 AND Sex = 'f'
 GROUP by Ethnicity
 ) as B 
